7d04eafae55b4556e8c3d222802aaf080ebf599b
emilianogastaldo <emiliano0303@gmail.com>
2024-02-23 17:20:05 +0100

    added cast to movie

--- index.php
+++ index.php
@@ -9,14 +9,16 @@ class Movie{
     public $vote;
     public $release_date;
     public $director;
+    public $cast;
 
-    public function __construct(String $title, String $overview, Int $vote, String $release_date, Director $director){
+    public function __construct(String $title, String $overview, Int $vote, String $release_date, Director $director, Array $cast){
         $this->id = uniqid();
         $this->title = $title;
         $this->overview = $overview;
         $this->vote = $vote;
         $this->release_date = $release_date;
         $this->director = $director;
+        $this->cast = $cast;
     }
 
     public function getTitle(){
@@ -77,17 +79,18 @@ $luisa = new Actor ('Luisa', 'Gialli', '31/01/1995', 'Main');
 $gennaro = new Actor('Gennaro', 'Costa', '23/07/1990', 'Main');
 $annalisa = new Actor ('Annalisa', 'Rossi', '15/03/1987', 'Secondary');
 $guglielmo = new Actor ('Guglielmo', 'Perusi', '15/06/1967', 'Secondary');
+$elisa = new Actor ('Elisa', 'Monti', '15/04/1977', 'Secondary');
+$sofia = new Actor ('Sofia', 'Leoni', '15/08/1997', 'Secondary');
 
 // Creo un array per il cast
-$cast = [$luisa, $gennaro, $annalisa, $guglielmo];
-var_dump($cast);
+$cast1 = [$luisa, $annalisa, $guglielmo];
+$cast2 = [$gennaro, $elisa, $sofia];
 
 // Creo i movies
-$movie1 = new Movie($title1, $overview1, $vote1, $release_date1, $giorgio);
-$movie2 = new Movie($title2, $overview2, $vote2, $release_date2, $emanuele);
+$movie1 = new Movie($title1, $overview1, $vote1, $release_date1, $giorgio, $cast1);
+$movie2 = new Movie($title2, $overview2, $vote2, $release_date2, $emanuele, $cast2);
 
 var_dump($movie1);
 var_dump($movie2);
-var_dump($giorgio);
 
 ?>
